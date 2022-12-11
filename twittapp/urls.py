5281f0da772697ff13c37b21e32ca7103eeded20
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2022-12-11 23:47:02 +0700

    Change urls.py

--- twittapp/urls.py
+++ twittapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('twitts', views.twitt_list_view, name = 'list'),
     path('twitts/<int:twitt_id>', views.twitt_detailed_view, name = 'detailed'),
     path('twittit', views.twitt_create_view, name = 'create'),
-    path('api/twitts/<int:twitt_id>/delete', views.twitt_delete_view, name = 'delete'),
-    path('api/twitts/action', views.twitt_like_view, name = "twittaction"),
+
+    #APIs
+    path('<int:twitt_id>/delete', views.twitt_delete_view, name = 'delete'),
+    path('action', views.twitt_like_view, name = "twittaction"),
 ]
